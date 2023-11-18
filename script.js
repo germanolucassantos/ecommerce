d9823df4d330c523c46f966d48b85eaab4d0375a
GERMANO LUCAS DA SILVA SANTOS <GERMANO0436@DIGITALCOLLEGE.LOCAL>
2023-11-17 21:59:39 -0300

    meu primeiro commit

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-var action = document.getElementById("criar") // selecionando o botão
-action.addEventListener("click", carregar) // escutando o evento click do botão
-var tabuada = document.getElementById("tabuada") // selecionando a div tabuada
+var lista = document.getElementById("lista") // selecionando o botão
 
 function carregar() {
-    var numero = window.document.getElementById("numero") // selecionando o numero digitado no in
-    tabuada.innerText = "";
-
     for(let i = 1; i <= 10; i++) {
-    tabuada.innerHTML += `${numero.value} * ${i} = ${numero.value * i}</br>`; // escrevendo o resultado
-    console.log("teste")
-   } 
+    lista. innerHTML +=
+ `<div class="col">
+
+ </div>`
+}
 }
